fced6f895ab741e933b240bdcad47cd20ddfa4ca
Simao Novais <snovaisg.97@gmail.com>
2019-11-24 11:19:03 +0000

    Started EDA-Simao, utils.py

---
+++ utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def save_predictions(predictions: pd.DataFrame, output_path: str):
+    """Save predictions to csv.
+    Saves the predictions into a csv file with the format we need.
+    We keep the index since it contains the user ids.
+    Args:
+        predictions (pd.DataFrame): DataFrame with user_id as index and ordered recommendations in the columns.
+        output_path (str): Filepath for the predictions file.
+    """
+    
+    predictions = predictions.copy()
+    cols_to_drop = predictions.columns
+    predictions['purchases'] = predictions.apply(lambda row: str([ str(row[column]) for column in df.columns]), axis=1)
+    predictions = predictions.drop(columns=cols_to_drop)
+    predictions.to_csv(output_path,index_label='user_id')
+    print(f"Saved to csv in '{output_path}'.")
